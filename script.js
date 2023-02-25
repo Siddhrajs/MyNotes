48faba2038e0ae90863f8ce22444e6cba6574cb5
Siddhrajs <siddhrajsisodiya7@gmail.com>
2023-02-26 00:26:36 +0530

    auto commit

--- script.js
+++ script.js
@@ -26,13 +26,4 @@ function myInitialise() {
     btnText.innerHTML = "Read more";
     moreText.style.display = "none";
   }
-  /*const fs = require('fs');
-
-  fs.readFile('./notes.txt', 'utf8', (err,data) => {
-      if(err) {
-        console.log(err);
-        return;
-      }
-      console.log(data);
-  });*/
 }
